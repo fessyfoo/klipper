a1d51f7015d38c49344b7cbf2528f60ef16d612c
fess <fess@fess.org>
2019-05-14 04:36:57 +0000

    retries: fixup
    config values need to be read at config time.
    bunch of other totally broken things.

--- klippy/extras/quad_gantry_level.py
+++ klippy/extras/quad_gantry_level.py
@@ -10,6 +10,8 @@ class QuadGantryLevel:
     def __init__(self, config):
         self.config  = config
         self.printer = config.get_printer()
+        self.retries = RetryHelper(config)
+        self.retries.error_msg_extra = "Possibly Z motor numbering is wrong"
         self.max_adjust = config.getfloat("max_adjust", 4, above=0)
         self.horizontal_move_z = config.getfloat("horizontal_move_z", 5.0)
         self.printer.register_event_handler("klippy:connect",
@@ -43,14 +45,8 @@ class QuadGantryLevel:
     cmd_QUAD_GANTRY_LEVEL_help = (
         "Conform a moving, twistable gantry to the shape of a stationary bed")
     def cmd_QUAD_GANTRY_LEVEL(self, params):
-
-        self.retries = RetryHelper(
-            self.config,
-            retry = lambda: self.probe_helper.start_probe(params))
-
-        self.retries.error_msg_extra = "Possibly Z motor numbering is wrong"
-
-        self.retries.start()
+        self.retries.start(
+            params, lambda: self.probe_helper.start_probe(params))
 
     def probe_finalize(self, offsets, positions):
         # Mirror our perspective so the adjustments make sense
@@ -111,7 +107,7 @@ class QuadGantryLevel:
             raise
 
         largest_adj = max([ abs(z) for z in z_adjust])
-        self.retries.check(largest_adjust)
+        self.retries.check(largest_adj)
 
 
     def linefit(self,p1,p2):
@@ -152,71 +148,78 @@ class RetryHelper:
     value_label     = "value"
     error_msg_extra = ""
 
-    def __init__(config, retry):
-        self.retry_function     = retry
+    def __init__(self, config):
 
-        default_retries         = config.getint("retries", 0)
-        default_retry_tolerance = config.getfloat("retry_tolerance", 0)
+        self.default_retries         = config.getint("retries", 0)
+        self.default_retry_tolerance = config.getfloat("retry_tolerance", 0)
 
+        printer    = config.get_printer()
+        self.gcode = printer.lookup_object('gcode')
+
+    def retry(self):
+        self.retry_function()
+
+    def start(self, params, retry_function):
         retries = self.gcode.get_int(
             'RETRIES', 
             params,
-            default=config.getint("retries", 0),
+            default=self.default_retries,
             minval=0,
             maxval=30)
 
         tolerance = self.gcode.get_float(
             'RETRY_TOLERANCE', 
             params, 
-            default=config.getfloat("retry_tolerance", 0),
+            default=self.default_retry_tolerance,
             minval=0, 
             maxval=1.0)
 
         self.retry_tolerance   = tolerance
         self.retries_remaining = retries
+        self.retries_total     = retries
         self.enabled           = retries > 0
-        printer                = config.get_printer()
-        self.gcode             = printer.lookup_object('gcode')
+        self.cnt               = 0
+        self.retry_function    = retry_function
+        self.previous          = None
+        self.retry()
 
-
-    def retry():
-        self.retry_function()
-
-    def start():
-        retry()
-
-    def error(value):
+    def error(self,value):
         if self.previous and value > self.previous + 0.0000001:
             return True
 
         self.previous = value
         return False
 
-    def good(value):
+    def good(self,value):
         return value <= self.retry_tolerance
 
-    def check(value):
+    def check(self,value):
         if not self.enabled:
             return
 
-        if error(value):
+        if self.error(value):
             self.gcode.respond_error(
                 "WARNING: %s of " % (self.value_label) +
                 "%0.6f is worse than previous %0.6f " %
-                (value, self.previous) + error_msg_extra)
+                (value, self.previous) + self.error_msg_extra)
             return
 
-        if good(value):
+        if self.good(value):
             return
+
+        self.cnt += 1
+        self.gcode.respond_info(
+            "Try: %d/%d " % (self.cnt, self.retries_total + 1) +
+            "%s: " % (self.value_label) +
+            "%0.6f tolerance: %0.6f" % (value, self.retry_tolerance))
 
         if self.retries_remaining > 0:
-            self.gcode.respond_info(
-                "retries %d %s: " % ( self.retries, self.value_label) +
-                "%0.6f tolerance: %0.6f" % (value, self.retry_tolerance))
             self.retries_remaining -= 1
             self.retry()
         else:
-            self.gcode.respond_error("Retries failed to converge target value")
+            self.gcode.respond_error(
+                "Retries: %s failed to converge to tolerance" %
+                (self.value_label))
 
 
 def load_config(config):
